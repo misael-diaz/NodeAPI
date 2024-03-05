7a43897393994ab9d0a5e64fb27320ffae39fc9e
misael-diaz <20134959+misael-diaz@users.noreply.github.com>
2024-03-04 21:32:08 -0400

    listen on specified port

---
+++ index.js
@@ -0,0 +1,21 @@
+const { listen } = require("./http")
+
+const NodeAPI = async () => {
+	await listen()
+}
+
+NodeAPI()
+
+/*
+
+NodeAPI							March 3, 2024
+
+source: index.js
+author: @misael-diaz
+
+Copyright (c) 2024 Misael Díaz-Maldonado
+This file is released under the GNU General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+*/
